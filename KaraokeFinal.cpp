c1cbd076acde233f7a39fa15c5bd1452960cb0cf
MystFreed <36942430+MystiFreed@users.noreply.github.com>
2019-11-16 17:58:48 -0600

    Updates to add menu to KaraokeFinal.cpp, add Artists.txt, update to 'class' in Genre.h

--- KaraokeFinal.cpp
+++ KaraokeFinal.cpp
@@ -10,13 +10,18 @@
 #include "CatalogEntry.h"
 #include "maps.h"
 #include <map>
+
 void test();
 void startup();
+bool displayMenu();
+
 int main()
 {
 	startup();
-	test();
-	
+	//test(); this is currently under the catalogue management menu as a placeholder
+	while (displayMenu()); //automatically repeats until a false (exit) is returned.
+
+	return 0;
 
 };
 
@@ -26,6 +31,52 @@ void startup() {
 	primaryMapFromFile(songMap, songFstream);
 	primaryMapFromFile(artistMap, artistFstream);
 	cout << "\nDone importing map data";
+
+
+};
+
+// Function displays a menu for user selection of the submenu. Returns true if menu should continue.
+bool displayMenu()
+{
+	char userSelection; //user choice within the top-of-house menu display
+
+	string prompt = "\n----Karaoke Role Selection Menu----\n ";
+	prompt += "A) Catalogue Management\n "; //this holds the menu options specific to management of the song/artist catalogues
+	prompt += "B) KJ Queue Management\n "; //this menu holds options for the KJ to manage the queue of singers
+	prompt += "C) Singer Menu\n "; //this menu holds singer options - histories, etc
+	prompt += "D) Exit program\n ";
+	prompt += "Please make a selection:\n ";
+	userSelection = getInputReprompt(prompt, 'A', 'D');//getInputPreprompt converts any entry to upper for comparison
+
+	//call the MenuMember() menu
+	if (toupper(userSelection) == 'A')
+	{
+		test();
+		return true;//causes main menu to continue
+	}
+
+	else if (toupper(userSelection) == 'B')
+	{
+		cout << "Add MenuInventory()" << endl;
+		return true;//causes main menu to continue
+	}
+	//call the MenuArt() menu
+	else if (toupper(userSelection) == 'C')
+	{
+		//MenuArt();
+		return true;//causes main menu to continue
+
+	}
+	//currently a stub with re-call to the displayMenu()
+	else if (toupper(userSelection) == 'D')
+	{
+		cout << "Thank you, program closing." << endl;
+		return false;//causes main menu to stop
+	}
+	else {
+		cout << "Main menu error. ";
+		return true;
+	}
 };
 
 //run this to test basic functions after changes
@@ -61,13 +112,4 @@ void test() {
 	displayMap(testMap);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
